d35b50c6a0eacffba8064affb57cfa8f0bab2fa4
Eric Davis <edavis@littlestreamsoftware.com>
2010-01-01 17:12:52 -0800

    Working through chapter 3

---
+++ ch3-sequential-programming/lib_misc.erl
@@ -0,0 +1,17 @@
+-module(lib_misc).
+-export([sum/1, for/3, qsort/1]).
+
+sum(L) ->  sum(L, 0).
+
+sum([], N) ->  N;
+sum([H|T], N) -> sum(T, H+N).
+
+for(Max, Max, F) -> [F(Max)];
+for(I, Max, F) -> [F(I)|for(I+1, Max, F)].
+
+qsort([]) ->
+    [];
+qsort([Pivot|T]) ->
+    qsort([X || X <- T, X < Pivot])
+        ++ [Pivot] ++
+        qsort([X || X <- T, X >= Pivot]).
